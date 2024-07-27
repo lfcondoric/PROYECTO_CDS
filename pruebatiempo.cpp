3c5e3dadb99f6fd3dae083add3fad144d45fae38
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-07-27 16:36:33 -0500

    PRUEBATIEMPO: Imprimiendo Dia actual de la semana

--- pruebatiempo.cpp
+++ pruebatiempo.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
 #include<ctime>
+#include<string>
 using namespace std;
 int main(){
 	time_t now=time(0);
 	tm *time=localtime(&now);
-	cout<<"Hora: "<<time->tm_hour<<endl;
-	cout<<"Minuto: "<<time->tm_min<<endl;
-	cout<<"Segundo: "<<time->tm_sec<<endl;
+	
+	string dia_semana[20];
+	dia_semana[0]="Domingo";
+	dia_semana[1]="Lunes";
+	dia_semana[2]="Martes";
+	dia_semana[3]="Miercoles";
+	dia_semana[4]="Jueves";
+	dia_semana[5]="Viernes";
+	dia_semana[6]="Sabado";
+	
+	
+	cout<<"Dia de la semana: "<<dia_semana[time->tm_wday]<<endl;
 	return 0;
 }
