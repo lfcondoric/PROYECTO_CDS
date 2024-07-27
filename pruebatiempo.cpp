0d80addcf6144c5a5d283f78fed3904f4deda619
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-07-27 16:49:18 -0500

    PRUEBATIEMPO: Imprimiendo Fecha actual

--- pruebatiempo.cpp
+++ pruebatiempo.cpp
@@ -4,18 +4,13 @@
 using namespace std;
 int main(){
 	time_t now=time(0);
-	tm *time=localtime(&now);
+	tm *time=localtime(&now);//Establece la hora y fecha actual de acuerdo a la fecha y hora local de la pc
 	
-	string dia_semana[20];
-	dia_semana[0]="Domingo";
-	dia_semana[1]="Lunes";
-	dia_semana[2]="Martes";
-	dia_semana[3]="Miercoles";
-	dia_semana[4]="Jueves";
-	dia_semana[5]="Viernes";
-	dia_semana[6]="Sabado";
+	int year = 1900 + time->tm_year;//se suma 1900, porque la libreria toma en cuenta los años desde el año 1900
+	int month = 1 + time->tm_mon;
 	
 	
-	cout<<"Dia de la semana: "<<dia_semana[time->tm_wday]<<endl;
+	cout<<"Fecha: "<<time->tm_mday<<"/"<<month<<"/"<<year<<endl;
+	
 	return 0;
 }
