c4aa4c17aaf363436b7a6d280fbd083d89f3ec6a
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-07-27 16:27:24 -0500

    PRUEBATIEMPO: Imprimiendo Hora actual

--- pruebatiempo.cpp
+++ pruebatiempo.cpp
@@ -2,5 +2,10 @@
 #include<ctime>
 using namespace std;
 int main(){
+	time_t now=time(0);
+	tm *time=localtime(&now);
+	cout<<"Hora: "<<time->tm_hour<<endl;
+	cout<<"Minuto: "<<time->tm_min<<endl;
+	cout<<"Segundo: "<<time->tm_sec<<endl;
 	return 0;
 }
