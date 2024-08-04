0c1aaa6150792a0f911b8a6b7210cdd9abf627ef
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-08-03 20:04:43 -0500

    Añadiendo en función servicios: leer archivos de texto 4

--- PROYECTOCDS.cpp
+++ PROYECTOCDS.cpp
@@ -417,6 +417,14 @@ void servicios(){
 				break;
 			case 4:
 				system("cls");
+				archivos.open("d:\\fp\\proyecto_cds\\servicios\\obstetricia.txt",ios::in);//Abrir el archivos en modo lectura
+  				while(!archivos.eof()){//Mientras no sea el final del archivo
+					getline(archivos,texto);
+					cout<<texto<<endl;
+				}
+				archivos.close();
+				system("pause");
+				system("cls");
 				break;
 			case 5:
 				system("cls");
