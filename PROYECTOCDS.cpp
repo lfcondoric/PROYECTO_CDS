d28508a6d9572c3007f76a230dada0dc1d9c42cf
jamamanica <jamamanica@unjbg.edu.pe>
2024-08-03 23:09:12 -0500

    Agrega funcion anticoagulantes

--- PROYECTOCDS.cpp
+++ PROYECTOCDS.cpp
@@ -48,6 +48,8 @@ void citas(int a,int op);
 void farmacia(int a);
 void antibioticos(int a,int op);
 void analgesicos(int a,int op);
+void anestesicos(int a,int op);
+void anticoagulantes(int a,int op);
 void pedido(int a,int op,int m);
 
 int main(){
@@ -630,19 +632,19 @@ void farmacia(int a){
 					break;
 				case 2:
 					system("cls");
-					analgecicos();
+					analgesicos(a,op);
 					break;
 				case 3:
 					system("cls");
-					//anestesicos();
+					anestesicos(a,op);
 					break;
 				case 4:
 					system("cls");
-					//anticoagulantes();
+					anticoagulantes(a,op);
 					break;
 				case 5:
 					system("cls");
-					//antihipertensivos();
+					//antihipertensivos(a,op);
 					break;
 				case 0:
 					system("cls");
@@ -734,6 +736,35 @@ void anestesicos(int a,int op){
 		cout<<"1. Lidocaina"<<endl;
 		cout<<"2. Propofol"<<endl;
 		cout<<"3. Midazolam"<<endl;			
+		cout<<"0. Salir"<<endl;
+		cout<<"--------------------------------------"<<endl;
+		cout<<"Seleccione una opcion: ";cin>>m;
+		switch(m){
+			case 1:
+			case 2:
+			case 3:
+				system("cls");
+				pedido(a,op,m);
+				system("pause");
+				break;
+			case 0:
+				system("cls");
+				return;
+				break;
+			default:
+				system("cls");
+				break;
+		}
+	}while(m!=0);
+}
+void anticoagulantes(int a,int op){
+	int m;
+	do{
+		system("cls");
+		cout<<"------------ANTICOAGULANTES----------"<<endl;
+		cout<<"1. Heparina"<<endl;
+		cout<<"2. Warfarina"<<endl;
+		cout<<"3. Enoxaparina"<<endl;			
 		cout<<"0. Salir"<<endl;
 		cout<<"--------------------------------------"<<endl;
 		cout<<"Seleccione una opcion: ";cin>>m;
