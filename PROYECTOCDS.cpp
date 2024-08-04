a586996c82a2546dee593be7885b4604d21c6200
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-08-03 20:07:49 -0500

    Añadiendo en función servicios: leer archivos de texto 8

--- PROYECTOCDS.cpp
+++ PROYECTOCDS.cpp
@@ -461,6 +461,14 @@ void servicios(){
 				break;
 			case 8:
 				system("cls");
+				archivos.open("d:\\fp\\proyecto_cds\\servicios\\radiologia.txt",ios::in);//Abrir el archivos en modo lectura
+				while(!archivos.eof()){//Mientras no sea el final del archivo
+					getline(archivos,texto);
+					cout<<texto<<endl;
+				}
+				archivos.close();
+				system("pause");
+				system("cls");
 				break;
 			case 9:
 				system("cls");
