47518065e2028568125c7652b45169aeebb09223
atellezm1 <atellezm@unjbg.edu.pe>
2024-08-04 17:24:12 -0500

    Deacuerdo a cada caso se implementa el nombre de cada mes correspondiente

--- SERVICIOS/PROYECTOCDS.cpp
+++ SERVICIOS/PROYECTOCDS.cpp
@@ -711,51 +711,51 @@ void citas(int a,int op, int e){
 	us[a].citas[e].dni=us[a].dni;
 	switch(mes){
 		case 1:{
-
+			cout<<"-----------enero-----------"<<endl;
 		break;
 		}
 		case 2:{
-			
+			cout<<"----------febrero----------"<<endl;
 		break;
 		}
 		case 3:{
-			
+			cout<<"-----------marzo-----------"<<endl;
 		break;
 		}
 		case 4:{
-			
+			cout<<"-----------abril-----------"<<endl;
 		break;
 		}
 		case 5:{
-			
+			cout<<"------------mayo------------"<<endl;
 		break;
 		}
 		case 6:{
-			
+			cout<<"-----------junio-----------"<<endl;
 		break;
 		}
 		case 7:{
-			
+			cout<<"-----------julio-----------"<<endl;
 		break;
 		}
 		case 8:{
-			
+			cout<<"-----------agosto-----------"<<endl;
 		break;
 		}
 		case 9:{
-			
+			cout<<"----------septiembre----------"<<endl;
 		break;
 		}
 		case 10:{
-			
+			cout<<"----------octubre----------"<<endl;
 		break;
 		}
 		case 11:{
-			
+			cout<<"----------noviembre----------"<<endl;
 		break;
 		}
 		case 12:{
-			
+			cout<<"----------diciembre----------"<<endl;
 		break;
 		}
 	}
